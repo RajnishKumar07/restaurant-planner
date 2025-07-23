58f42579e5add3df1912d640f81b8e163b8639ac
Rajnish Kumar <rajnish6633@gmail.com>
2025-07-23 20:22:36 +0530

    feat: basic component created and routing setup

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./components/planner/planner.component').then(
+        (c) => c.PlannerComponent
+      ),
+  },
+];
